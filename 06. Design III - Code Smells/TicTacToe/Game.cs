f13478a6004a8e23fce07d968e0ebeff21079885
Anton Bevzuk <a.bevzuk@dodopizza.com>
2019-06-19 17:08:37 +0300

    Move to separate files Remove comments Remove duplications

--- 06. Design III - Code Smells/TicTacToe/Game.cs
+++ 06. Design III - Code Smells/TicTacToe/Game.cs
@@ -1,127 +1,72 @@
 ﻿using System;
-using System.Linq;
-using System.Collections.Generic;
 
 namespace TicTacToe
 {
-    public class Tile
-    {
-        public int X {get; set;}
-        public int Y {get; set;}
-        public char Symbol {get; set;}
-    }
-
-    public class Board
-    {
-       private List<Tile> _plays = new List<Tile>();
-       
-        public Board()
-        {
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    _plays.Add(new Tile{ X = i, Y = j, Symbol = ' '});
-                }  
-            }       
-        }
-       public Tile TileAt(int x, int y)
-       {
-           return _plays.Single(tile => tile.X == x && tile.Y == y);
-       }
-
-       public void AddTileAt(char symbol, int x, int y)
-       {
-           var newTile = new Tile
-           {
-               X = x,
-               Y = y,
-               Symbol = symbol
-           };
-
-           _plays.Single(tile => tile.X == x && tile.Y == y).Symbol = symbol;
-       }
-    }
-
     public class Game
     {
         private char _lastSymbol = ' ';
         private Board _board = new Board();
-        
+
         public void Play(char symbol, int x, int y)
         {
-            //if first move
-            if(_lastSymbol == ' ')
+            if (isFirstMove())
             {
-                //if player is X
-                if(symbol == 'O')
+                if (playerIsO(symbol))
                 {
                     throw new Exception("Invalid first player");
                 }
-            } 
-            //if not first move but player repeated
-            else if (symbol == _lastSymbol)
+            }
+            else if (isPlayerRepeated(symbol))
             {
                 throw new Exception("Invalid next player");
             }
-            //if not first move but play on an already played tile
-            else if (_board.TileAt(x, y).Symbol != ' ')
+            else if (playOnAlreadyPlayedTile(x, y))
             {
                 throw new Exception("Invalid position");
             }
 
-            // update game state
             _lastSymbol = symbol;
             _board.AddTileAt(symbol, x, y);
         }
 
-        public char Winner()
-        {   //if the positions in first row are taken
-            if(_board.TileAt(0, 0).Symbol != ' ' &&
-               _board.TileAt(0, 1).Symbol != ' ' &&
-               _board.TileAt(0, 2).Symbol != ' ')
-               {
-                    //if first row is full with same symbol
-                    if (_board.TileAt(0, 0).Symbol == 
-                        _board.TileAt(0, 1).Symbol &&
-                        _board.TileAt(0, 2).Symbol == 
-                        _board.TileAt(0, 1).Symbol )
-                        {
-                            return _board.TileAt(0, 0).Symbol;
-                        }
-               }
-                
-             //if the positions in first row are taken
-             if(_board.TileAt(1, 0).Symbol != ' ' &&
-                _board.TileAt(1, 1).Symbol != ' ' &&
-                _board.TileAt(1, 2).Symbol != ' ')
-                {
-                    //if middle row is full with same symbol
-                    if (_board.TileAt(1, 0).Symbol == 
-                        _board.TileAt(1, 1).Symbol &&
-                        _board.TileAt(1, 2).Symbol == 
-                        _board.TileAt(1, 1).Symbol)
-                        {
-                            return _board.TileAt(1, 0).Symbol;
-                        }
-                }
+        private bool playOnAlreadyPlayedTile(int x, int y)
+        {
+            return _board.TileAt(x, y).Symbol != ' ';
+        }
 
-            //if the positions in first row are taken
-             if(_board.TileAt(2, 0).Symbol != ' ' &&
-                _board.TileAt(2, 1).Symbol != ' ' &&
-                _board.TileAt(2, 2).Symbol != ' ')
+        private bool isPlayerRepeated(char symbol)
+        {
+            return symbol == _lastSymbol;
+        }
+
+        private static bool playerIsO(char symbol)
+        {
+            return symbol == 'O';
+        }
+
+        private bool isFirstMove()
+        {
+            return _lastSymbol == ' ';
+        }
+
+        public char Winner()
+        {
+            for (var row = 0; row <= 2; row++)
+            {
+                if (isRowFullWithSameSymbol(row))
                 {
-                    //if middle row is full with same symbol
-                    if (_board.TileAt(2, 0).Symbol == 
-                        _board.TileAt(2, 1).Symbol &&
-                        _board.TileAt(2, 2).Symbol == 
-                        _board.TileAt(2, 1).Symbol)
-                        {
-                            return _board.TileAt(2, 0).Symbol;
-                        }
+                    return _board.TileAt(row, 0).Symbol;
                 }
+            }
 
             return ' ';
+        }
+
+        private bool isRowFullWithSameSymbol(int row)
+        {
+            return _board.TileAt(row, 0).Symbol == _board.TileAt(row, 1).Symbol &&
+                   _board.TileAt(row, 2).Symbol == _board.TileAt(row, 1).Symbol &&
+                   _board.TileAt(row, 0).Symbol != ' ';
         }
     }
 }
---
+++ 06. Design III - Code Smells/TicTacToe/Tile.cs
@@ -0,0 +1,9 @@
+namespace TicTacToe
+{
+    public class Tile
+    {
+        public int X {get; set;}
+        public int Y {get; set;}
+        public char Symbol {get; set;}
+    }
+}
