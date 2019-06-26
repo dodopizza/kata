f1a601d42aa50974a0248aacf07055eeb9c41fbe
Anatoliy Sorokin <a.sorokin@dodopizza.com>
2019-06-26 15:19:32 +0300

    use var, format code

--- 05. Design II - Refactoring/03. Parallel change/ParallelAndNarrowChange/Tests/AuthenticatorShould.cs
+++ 05. Design II - Refactoring/03. Parallel change/ParallelAndNarrowChange/Tests/AuthenticatorShould.cs
@@ -2,20 +2,24 @@
 using NUnit.Framework;
 using ParallelAndNarrowChange.Method;
 
-namespace ParallelAndNarrowChange.Tests{
+namespace ParallelAndNarrowChange.Tests
+{
     [TestFixture]
-    public class AuthenticationServiceTest{
+    public class AuthenticationServiceTest
+    {
         [Test]
-        public void administrator_is_always_authenticated(){
+        public void administrator_is_always_authenticated()
+        {
             var service = new AuthenticationService();
             var adminId = 12345;
             service.IsAuthenticated(adminId).Should().BeTrue();
         }
 
         [Test]
-        public void normalUser_is_not_authenticated_initially(){
-            AuthenticationService service = new AuthenticationService();
-            int normalUserId = 11111;
+        public void normalUser_is_not_authenticated_initially()
+        {
+            var service = new AuthenticationService();
+            var normalUserId = 11111;
             service.IsAuthenticated(normalUserId).Should().BeFalse();
         }
     }
