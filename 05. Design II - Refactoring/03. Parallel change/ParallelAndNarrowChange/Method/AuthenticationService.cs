e28a7573f6d926bc1f50b90bf0a0edc978f3e188
Anatoliy Sorokin <a.sorokin@dodopizza.com>
2019-06-26 15:19:08 +0300

    remove usings, format code

--- 05. Design II - Refactoring/03. Parallel change/ParallelAndNarrowChange/Method/AuthenticationService.cs
+++ 05. Design II - Refactoring/03. Parallel change/ParallelAndNarrowChange/Method/AuthenticationService.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ParallelAndNarrowChange.Method {
-    public class AuthenticationService{
-        public bool IsAuthenticated(int id){
+﻿namespace ParallelAndNarrowChange.Method
+{
+    public class AuthenticationService
+    {
+        public bool IsAuthenticated(int id)
+        {
             return id == 12345;
         }
     }
