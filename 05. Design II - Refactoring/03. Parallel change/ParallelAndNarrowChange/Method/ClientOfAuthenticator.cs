16eeed255f85a9453575a39282d31d865151b999
Anatoliy Sorokin <a.sorokin@dodopizza.com>
2019-06-26 15:39:59 +0300

    refactor ClientOfAuthenticator

--- 05. Design II - Refactoring/03. Parallel change/ParallelAndNarrowChange/Method/ClientOfAuthenticator.cs
+++ 05. Design II - Refactoring/03. Parallel change/ParallelAndNarrowChange/Method/ClientOfAuthenticator.cs
@@ -1,24 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ParallelAndNarrowChange.Method {
-public class ClientOfAuthenticator {
-    private AuthenticationService authenticationService;
-
-    public static void Main(String[] args) {
-        new ClientOfAuthenticator(new AuthenticationService()).Run();
+namespace ParallelAndNarrowChange.Method
+{
+    public static class ClientOfAuthenticator
+    {
+        public static void Main()
+        {
+            var authenticationService = new AuthenticationService();
+            var authenticated = authenticationService.IsAuthenticated(33);
+            Console.WriteLine("33 is authenticated = " + authenticated);
+        }
     }
-
-    public ClientOfAuthenticator(AuthenticationService authenticationService) {
-        this.authenticationService = authenticationService;
-    }
-
-    public void Run() {
-        bool authenticated = authenticationService.IsAuthenticated(33);
-        Console.WriteLine("33 is authenticated = " + authenticated);
-    }
-}
 }
