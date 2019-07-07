4e84d6917367270aa797587e19466f1bd6dc97b8
anatoly.sorokin <anatoly.sorokin.personal@gmail.com>
2019-07-07 18:13:02 +0300

    WhenProcess_ThenTaskPassedToInstrument

--- 07. Test Doubles/02. Instrument Processor/Domain/InstrumentProcessor.cs
+++ 07. Test Doubles/02. Instrument Processor/Domain/InstrumentProcessor.cs
@@ -2,16 +2,19 @@ namespace Domain
 {
     public class InstrumentProcessor : IInstrumentProcessor
     {
+        private readonly IInstrument _instrument;
         private readonly ITaskDispatcher _taskDispatcher;
 
         public InstrumentProcessor(IInstrument instrument, ITaskDispatcher taskDispatcher)
         {
+            _instrument = instrument;
             _taskDispatcher = taskDispatcher;
         }
 
         public void Process()
         {
-            _taskDispatcher.GetTask();
+            var task = _taskDispatcher.GetTask();
+            _instrument.Execute(task);
         }
     }
 }
