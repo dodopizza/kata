a449cae29612a89c7ace1dce09ee173ba6c76c46
dasha <d.bayanova@dodopizza.com>
2019-07-10 16:50:38 +0300

    WhenUpdateQuality - ForRegularItem_QualityDecrements

---
+++ 08. Testing Legacy Code/01. Gilded Rose/App/WhenUpdateQuality.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace App
+{
+    [TestFixture]
+    public class WhenUpdateQuality
+    {
+        [Test]
+        public void ForRegularItem_QualityDecrements()
+        {
+            var items = new List<Item> {new Item {Name = "Any", SellIn = 1, Quality = 200}};
+            var app = new GildedRose(items);
+
+            app.UpdateQuality();
+            
+            Assert.AreEqual(199, items.Single().Quality);
+        }
+    }
+}
