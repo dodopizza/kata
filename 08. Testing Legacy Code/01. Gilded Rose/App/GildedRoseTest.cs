da4e0eab3565b69c05b030f70830d7b2791a27ef
chernetsova tatyana <t.chernetsova@dodopizza.com>
2019-07-10 16:49:17 +0300

    WhenNonSpecificName_ThenQualityAndSellInReduceOnOne

--- 08. Testing Legacy Code/01. Gilded Rose/App/GildedRoseTest.cs
+++ 08. Testing Legacy Code/01. Gilded Rose/App/GildedRoseTest.cs
@@ -12,7 +12,27 @@ namespace App
             IList<Item> Items = new List<Item> {new Item {Name = "foo", SellIn = 0, Quality = 0}};
             var app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+            Assert.AreEqual("foo", Items[0].Name);
+        }
+
+        [Test]
+        public void WhenNonSpecificName_ThenQualityAndSellInReduceOnOne()
+        {
+            var item = new Item
+            {
+                Name = "name",
+                Quality = 1,
+                SellIn = 1
+            };
+            var items = new[] {item};
+            var gildedRose = new GildedRose(items);
+            
+            gildedRose.UpdateQuality();
+
+            var expected = new Item {Name = "name", Quality = 0, SellIn = 0};
+            var actual = item;
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
--- 08. Testing Legacy Code/01. Gilded Rose/App/Item.cs
+++ 08. Testing Legacy Code/01. Gilded Rose/App/Item.cs
@@ -1,6 +1,8 @@
-﻿namespace App
+﻿using System;
+
+namespace App
 {
-    public class Item
+    public class Item : IEquatable<Item>
     {
         public string Name { get; set; }
         public int SellIn { get; set; }
@@ -9,6 +11,32 @@
         public override string ToString()
         {
             return Name + ", " + SellIn + ", " + Quality;
+        }
+
+        public bool Equals(Item other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return string.Equals(Name, other.Name) && SellIn == other.SellIn && Quality == other.Quality;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Item) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (Name != null ? Name.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ SellIn;
+                hashCode = (hashCode * 397) ^ Quality;
+                return hashCode;
+            }
         }
     }
 }
