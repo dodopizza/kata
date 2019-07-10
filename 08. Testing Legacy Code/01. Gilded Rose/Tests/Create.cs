cd600360a01430434c95825a56e1f51d2c161690
dasha <d.bayanova@dodopizza.com>
2019-07-10 17:10:51 +0300

    Refactor

--- 08. Testing Legacy Code/01. Gilded Rose/Tests/Create.cs
+++ 08. Testing Legacy Code/01. Gilded Rose/Tests/Create.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 namespace App
 {
     public class Create
-    {
-        public static IList<Item> RegularItem(int sellIn, int quality)
-        {
-            return new List<Item> {new Item {Name = "Any", SellIn = sellIn, Quality = quality}};
-        }
-
+    { 
         public static IList<Item> AgedBrie(int sellIn, int quality)
         {
-            return new List<Item> {new Item {Name = "Aged Brie", SellIn = sellIn, Quality = quality}};
+            return Items("Aged Brie", sellIn, quality);
+        }
+        
+        public static IList<Item> RegularItem(int sellIn, int quality)
+        {
+            return Items("Any", sellIn, quality);
+        }
+        
+        private static List<Item> Items(string name, int sellIn, int quality)
+        {
+            return new List<Item> {new Item {Name = name, SellIn = sellIn, Quality = quality}};
         }
     }
 }
